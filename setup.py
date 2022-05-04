eafb0ae7b6660b91867fd390c3e9a86226763dc4
pear-c <100738513+pear-c@users.noreply.github.com>
2022-05-04 09:26:31 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup (
   author='sunghwanbae',
   author_email='zxcv9676@naver.com',
   url = "https://github.com/pear-c/mycalc1",
-  download_url =
+  download_url = "https://github.com/pear-c/mycalc1/releases/tag/v1.0.0",
   install_requires=['pytest'],
   long_description = 'oss-dev calculator python module',
   long_description_content_type = 'text/markdown',
